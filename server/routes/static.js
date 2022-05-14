aaf89e6166a008108fd18971c3cc6ede705a5015
Ojaswi Athghara <ojaswi.athghara98@gmail.com>
2022-05-14 08:25:58 +0530

    full working app state 1

--- server/routes/static.js
+++ server/routes/static.js
@@ -2,10 +2,11 @@ const { Router } = require("express");
 const path = require("path");
 
 let staticRouter = Router();
-// const buildPath = path.join(__dirname, "../../build");
+const buildPath = path.join(__dirname, "../../build");
 
 staticRouter.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "test.html"));
+    console.log(buildPath);
+    res.sendFile(path.join(buildPath, "index.html"));
 });
 
 module.exports = staticRouter;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const path = require("path");
+const path = require("path");
 
+// route files
 const staticRoutes = require("./routes/static");
 const crudRoutes = require("./routes/crud");
 
+// setup variables
+const port = process.env.PORT || 16500;
+const buildPath = path.join(__dirname, "../build");
+
 const app = express();
+
+// setup encoders and authentication
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.PORT || 16500;
-// const buildPath = path.join(__dirname, "../build");
+// static paths
+app.use(express.static(buildPath));
 
-// app.use(express.static(buildPath));
-
+// routes
 app.use("/", staticRoutes);
 app.use("/crud", crudRoutes);
 
